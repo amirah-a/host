e876e8d7ea9f3c8edf6c4b9693377423a0296d41
amiraha-sta <amirah.ali2@my.uwi.edu>
2026-02-25 16:59:30 -0400

    Update Cohorts

--- resources/views/form/cohorts.blade.php
+++ resources/views/form/cohorts.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <h2 class="font-bold text-gray-900 mb-2">This course will be delivered in 3 cohorts, Tuesday and Thursdays from 3:00pm to 6:00pm.</h2>
+    <h2 class="font-bold text-gray-900 mb-2">This course will be delivered in 2 cohorts. Please, take note of your cohort number when selecting an option.</h2>
     <div class="overflow-x-auto">
         <table class="min-w-full bg-white border border-gray-200 rounded-lg shadow-sm">
             <thead class="bg-gray-50">
@@ -13,22 +13,17 @@
             <tbody class="divide-y divide-gray-200">
                 <tr class="hover:bg-gray-50">
                     <td class="px-3 sm:px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-                    <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">Tuesday 14th October &<br>Thursday 16th October 2025</td>
-                    <td class="px-3 sm:px-6 py-4 whitespace-nowrap text-sm text-gray-900">3:00pm – 6:00pm</td>
-                    <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">Los Bajos Youth Development Centre</td>
+                    <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">Tuesdays and Thursdays</td>
+                    <td class="px-3 sm:px-6 py-4 whitespace-nowrap text-sm text-gray-900">5:00pm – 8:00pm</td>
+                    <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">Wallerfield Activity Centre</td>
                 </tr>
                 <tr class="hover:bg-gray-50">
                     <td class="px-3 sm:px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
-                    <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">Wednesday 15th October &<br>Wednesday 22nd October 2025</td>
-                    <td class="px-3 sm:px-6 py-4 whitespace-nowrap text-sm text-gray-900">3:00pm – 6:00pm</td>
-                    <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">St James Youth Development Centre</td>
-                </tr>
-                <tr class="hover:bg-gray-50">
-                    <td class="px-3 sm:px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
-                    <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">Tuesday 21st October &<br>Thursday 23rd October 2025</td>
-                    <td class="px-3 sm:px-6 py-4 whitespace-nowrap text-sm text-gray-900">3:00pm – 6:00pm</td>
+                    <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">Mondays and Wednesdays</td>
+                    <td class="px-3 sm:px-6 py-4 whitespace-nowrap text-sm text-gray-900">5:00pm – 8:00pm</td>
                     <td class="px-3 sm:px-6 py-4 text-sm text-gray-900">California Youth Development Centre</td>
                 </tr>
+
             </tbody>
         </table>
     </div>
