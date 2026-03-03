bdc438fec7d180a25f670d9343dd4f6bbed8a0c8
amiraha-sta <amirah.ali2@my.uwi.edu>
2026-03-03 01:00:09 -0400

    Update send mail command

--- app/Console/Commands/SendAccessCodes.php
+++ app/Console/Commands/SendAccessCodes.php
@@ -2,10 +2,9 @@
 
 namespace App\Console\Commands;
 
-use App\Mail\AccessCodeMail;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Http;
 
 class SendAccessCodes extends Command
 {
